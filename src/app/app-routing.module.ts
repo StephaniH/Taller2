b0669f29ea6aaecdfd8f9984dcc7a51b8db1757b
CDavid07 <73976024+CDavid07@users.noreply.github.com>
2022-04-26 17:01:06 -0500

    Navegacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { P1Component } from './juegos/pages/p1/p1.component';
 import { P2Component } from './juegos/pages/p2/p2.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent },
+  { path: '', pathMatch: 'full', redirectTo: "principal" },
   { path: 'ppt', component: P1Component },
   { path: 'gato', component: P2Component },
-  { path: '**', redirectTo: '' }
+  { path: 'principal', component: SidebarComponent}
+  
 ];
 
 @NgModule({
